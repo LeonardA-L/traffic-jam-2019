0aea95de95e9597b85609cb0ae3c5615f6b3433d
LeonardA-L <leonard@allain-launay.fr>
2019-01-31 21:51:06 +0100

    Add Scene streaming draft

---
+++ Assets/Editor/Scripts/SceneBeaconEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SceneBeacon))]
+public class SceneBeaconEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        SceneBeacon beacon = (SceneBeacon)target;
+
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(beacon.transform.position, new Vector3(0, 1, 0), beacon.m_enableRadius);
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(beacon.transform.position, new Vector3(0, 1, 0), beacon.m_disableRadius);
+    }
+}
---
+++ Assets/Scripts/SceneBeacon.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBeacon : MonoBehaviour
+{
+    public float m_enableRadius = 3;
+    public float m_disableRadius = 4;
+    public string m_sceneToLoad = "Scenes/";
+
+    private Transform m_character;
+    private bool m_enabled = false;
+    //private List<GameObject> m_loadedRoots;
+
+    void Start()
+    {
+        //m_loadedRoots = new List<GameObject>();
+        Debug.Assert(m_enableRadius < m_disableRadius, "enableRadius should be < to disableRadius", gameObject);
+        // TODO properly get player transform
+        var character = (LeikirTest.CharacterController)FindObjectOfType(typeof(LeikirTest.CharacterController)); // Assume there is only one character
+        Debug.Assert(character != null, "No LeikirTest.CharacterController found on scene");
+        m_character = character.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sqrDist = (transform.position - m_character.position).sqrMagnitude;
+        if(!m_enabled && sqrDist <= m_enableRadius * m_enableRadius)
+        {
+            StartCoroutine(LoadSceneAsync(m_sceneToLoad));
+            m_enabled = true;
+        } else if(m_enabled && sqrDist >= m_disableRadius * m_disableRadius)
+        {
+            StartCoroutine(UnloadSceneAsync(m_sceneToLoad));
+            m_enabled = false;
+        }
+    }
+
+    IEnumerator LoadSceneAsync(string _scene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(_scene, LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        /*
+        Scene loaded = SceneManager.GetSceneByName(_scene);
+        if (!loaded.isLoaded)
+            yield return null;
+        if(!loaded.IsValid())
+            yield return null;
+        var roots = loaded.GetRootGameObjects();
+        m_loadedRoots.AddRange(roots);
+
+        foreach(GameObject root in m_loadedRoots)
+        {
+            Debug.Log(root.name);
+            root.transform.position += transform.position;
+        }
+        */
+    }
+    IEnumerator UnloadSceneAsync(string _scene)
+    {
+        AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync(_scene);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
