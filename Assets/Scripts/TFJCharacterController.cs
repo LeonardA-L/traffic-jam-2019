5c5ef47d4557295cfe34623b25dfe680728d184d
LeonardA-L <leonard@allain-launay.fr>
2019-02-04 18:05:20 +0100

    Draft gamepad controller

--- Assets/Scripts/TFJCharacterController.cs
+++ Assets/Scripts/TFJCharacterController.cs
@@ -9,14 +9,21 @@ namespace tfj
     public class TFJCharacterController : MonoBehaviour
     {
         public string m_groundLayer = "Water";
+        public string m_verticalAxis = "Vertical";
+        public string m_horizontalAxis = "Horizontal";
+        public Transform m_camera;
+        public float m_commandOffset = 7;
         [SerializeField]
         private bool m_playerMoving = false;
         private NavMeshAgent m_playerAgent;
+
+        private Vector3 m_command;
 
         // Start is called before the first frame update
         void Start()
         {
             m_playerAgent = GetComponent<NavMeshAgent>();
+            Debug.Assert(m_camera != null, "No Camera provided.", this);
         }
 
         // Update is called once per frame
@@ -39,6 +46,23 @@ namespace tfj
                     }
                 }
             }
+
+            // Fetch gamepad input
+            Vector2 joy = new Vector2(Input.GetAxis(m_horizontalAxis), Input.GetAxis(m_verticalAxis));
+            float magnitude = Mathf.Clamp(joy.magnitude, 0, 1);
+            joy.Normalize();
+            joy *= magnitude;
+
+            Vector3 camForward = m_camera.forward;
+            camForward.y = 0;
+            //Debug.Assert(camForward.sqrMagnitude != 0);   // With the hack'n'slash view we assume we don't need this
+            camForward.Normalize();
+
+            m_command = transform.position
+                      + (joy.y * camForward + joy.x * m_camera.right)
+                      * m_commandOffset;
+
+            SetPlayerAction(m_command);
         }
 
         public void SetPlayerAction(Vector3 goal)
@@ -49,6 +73,13 @@ namespace tfj
             }
             m_playerMoving = true;
             m_playerAgent.SetDestination(goal);
+        }
+
+        void OnDrawGizmos()
+        {
+            // Draw a yellow sphere at the transform's position
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(m_command, 1);
         }
     }
 }
