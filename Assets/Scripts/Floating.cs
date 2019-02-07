185d70f19196d67a11dc5ef0c82afa080f32497a
LeonardA-L <leonard@allain-launay.fr>
2019-02-07 18:09:09 +0100

    Tweak floating and navmesh

--- Assets/Scripts/Floating.cs
+++ Assets/Scripts/Floating.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class Floating : MonoBehaviour
 {
-    public float m_phase = 0;
+    public float m_phase = 2;
     public float m_amplitude = 1;
-    public float m_waveSpeed = 4.4f;
 
-    private float m_origY;
+    public float m_waveSpeed = 0.3f;
+    public float m_waveAmplitude = 3.0f;
+    public float m_waveIntensity = 5f;
+
+    private Vector3 m_offsetY;
     // Start is called before the first frame update
     void Start()
     {
-        m_origY = transform.position.y;
+        m_offsetY = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, m_origY + m_amplitude * Mathf.Sin((m_phase * transform.position.x + Time.timeSinceLevelLoad) * m_waveSpeed), transform.position.z);
+        transform.position -= m_offsetY;
+
+        float timeFactor = Mathf.Sin(Time.timeSinceLevelLoad * m_waveSpeed + m_phase * transform.position.x);
+        float Waves = (timeFactor * m_amplitude * m_waveIntensity * 10.0f);
+        m_offsetY = new Vector3(0, Waves * 0.04f, 0);
+
+        transform.position += m_offsetY;
     }
 }
