31460441bf03050c66c5bdea8da36e854a029343
LeonardA-L <leonard@allain-launay.fr>
2019-02-07 19:12:28 +0100

    Add random roll

---
+++ Assets/BoatVisualEffects.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tfj
+{
+    public class BoatVisualEffects : MonoBehaviour
+    {
+        public TFJCharacterController m_controller;
+        public float m_rollFrequency = 1;
+        public float m_rollAmplitude = 4;
+        private Rigidbody m_rigidbody;
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_rigidbody = m_controller.GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float noise = m_rollAmplitude * (2 * Mathf.PerlinNoise(m_rollFrequency * Time.timeSinceLevelLoad, m_rollFrequency * Time.timeSinceLevelLoad) - 0.5f);
+
+            Vector3 horizon = transform.right;
+            horizon.y = 0;
+            horizon.Normalize();
+
+            Vector3 to = (horizon + noise * Vector3.up).normalized;
+
+            Debug.DrawLine(transform.position, transform.position + horizon * 10, Color.red);
+            Debug.DrawLine(transform.position, transform.position + transform.right * 10, Color.blue);
+
+            float roll = Vector3.SignedAngle(horizon, to, transform.forward);
+
+            transform.localRotation = Quaternion.Euler(0, 0, roll);
+        }
+    }
+}
--- Assets/Scripts/Floating.cs
+++ Assets/Scripts/Floating.cs
@@ -2,31 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Floating : MonoBehaviour
+namespace tfj
 {
-    public float m_phase = 2;
-    public float m_amplitude = 1;
-
-    public float m_waveSpeed = 0.3f;
-    public float m_waveAmplitude = 3.0f;
-    public float m_waveIntensity = 5f;
-
-    private Vector3 m_offsetY;
-    // Start is called before the first frame update
-    void Start()
+    public class Floating : MonoBehaviour
     {
-        m_offsetY = Vector3.zero;
-    }
+        public float m_phase = 2;
+        public float m_amplitude = 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position -= m_offsetY;
+        public float m_waveSpeed = 0.3f;
+        public float m_waveAmplitude = 3.0f;
+        public float m_waveIntensity = 5f;
 
-        float timeFactor = Mathf.Sin(Time.timeSinceLevelLoad * m_waveSpeed + m_phase * transform.position.x);
-        float Waves = (timeFactor * m_amplitude * m_waveIntensity * 10.0f);
-        m_offsetY = new Vector3(0, Waves * 0.04f, 0);
+        private Vector3 m_offsetY;
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_offsetY = Vector3.zero;
+        }
 
-        transform.position += m_offsetY;
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position -= m_offsetY;
+
+            float timeFactor = Mathf.Sin(Time.timeSinceLevelLoad * m_waveSpeed + m_phase * transform.position.x);
+            float Waves = (timeFactor * m_amplitude * m_waveIntensity * 10.0f);
+            m_offsetY = new Vector3(0, Waves * 0.04f, 0);
+
+            transform.position += m_offsetY;
+        }
     }
 }
