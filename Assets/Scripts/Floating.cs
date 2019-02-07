6d6f2cd78207f2186d4f508055bd4d4019a8327e
LeonardA-L <leonard@allain-launay.fr>
2019-02-07 12:56:05 +0100

    Floating objects draft

---
+++ Assets/Scripts/Floating.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    public float m_phase = 0;
+    public float m_amplitude = 1;
+    public float m_waveSpeed = 4.4f;
+
+    private float m_origY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_origY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, m_origY + m_amplitude * Mathf.Sin((m_phase * transform.position.x + Time.timeSinceLevelLoad) * m_waveSpeed), transform.position.z);
+    }
+}
