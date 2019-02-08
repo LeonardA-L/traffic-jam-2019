44585778da4b7442988a238236508e93715ce6d7
LeonardA-L <leonard@allain-launay.fr>
2019-02-08 11:51:45 +0100

    Import some VBG assets

---
+++ Assets/Scripts/CameraRegisterer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tfj
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraRegisterer : MonoBehaviour
+    {
+        public string m_name;
+        public bool m_activeByDefault = false;
+        void Start()
+        {
+            CameraManager.Instance.RegisterCamera(m_name, GetComponent<Camera>());
+            gameObject.SetActive(m_activeByDefault);
+        }
+    }
+}
