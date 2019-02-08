dba6a9e6915f6dbc595ef9acf7aab4f2034a0182
LeonardA-L <leonard@allain-launay.fr>
2019-02-08 19:25:27 +0100

    Add tooltip system

---
+++ Assets/Scripts/DockingArea.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tfj
+{
+    public class DockingArea : MonoBehaviour
+    {
+        public string m_dialogNode;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var controller = other.attachedRigidbody.gameObject.GetComponent<TFJCharacterController>();
+            if (controller == null)
+            {
+                return;
+            }
+
+            UIManager.Instance.ShowTooltip("Dock", TooltipController.TooltipType.A); // TODO External translation file
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            var controller = other.attachedRigidbody.gameObject.GetComponent<TFJCharacterController>();
+            if (controller == null)
+            {
+                return;
+            }
+
+            UIManager.Instance.HideTooltip();
+        }
+    }
+}
