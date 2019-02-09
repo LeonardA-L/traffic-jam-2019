286cb7452849881b2e739e7e388cac2c72eb1ee5
Basile_Mathieu <basilemathieu@gmail.com>
2019-02-09 16:38:59 +0100

    Fix InventoryUI loading

--- Assets/Scripts/Trading/SelectableOption.cs
+++ Assets/Scripts/Trading/SelectableOption.cs
@@ -6,69 +6,69 @@ using UnityEngine.EventSystems;
 
 public class SelectableOption : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
 {
-	Button button;
-	LayoutElement layoutElement;
-	public float MinimumWidth = 100f;
-	public float ExpandedWidth = 600f;
-	public string text;
-	Text shortText;
-	Text longText;
+    Button button;
+    LayoutElement layoutElement;
+    public float MinimumWidth = 100f;
+    public float ExpandedWidth = 600f;
+    public string text;
+    Text shortText;
+    Text longText;
 
-	void Awake()
-	{
-		button = GetComponent<Button>();
-		layoutElement = GetComponent<LayoutElement>();
-		layoutElement.minWidth = MinimumWidth;
-		shortText = GetComponentsInChildren<Text>(true)[0];
-		longText = GetComponentsInChildren<Text>(true)[1];
+    void Awake()
+    {
+        button = GetComponent<Button>();
+        layoutElement = GetComponent<LayoutElement>();
+        layoutElement.minWidth = MinimumWidth;
+        shortText = GetComponentsInChildren<Text>(true)[0];
+        longText = GetComponentsInChildren<Text>(true)[1];
 
 
-		if (this.gameObject == EventSystem.current.currentSelectedGameObject)
-		{
-			button.OnSelect(null);
-			layoutElement.minWidth = ExpandedWidth;
-			DisplayText(true);
+        if (this.gameObject == EventSystem.current.currentSelectedGameObject)
+        {
+            button.OnSelect(null);
+            layoutElement.minWidth = ExpandedWidth;
+            DisplayText(true);
 
-		}
-		else DisplayText(false);
+        }
+        else DisplayText(false);
 
-	}
+    }
 
-	public void OnPointerEnter(PointerEventData eventData)
-	{
-		if (!EventSystem.current.alreadySelecting)
-			button.Select();
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!EventSystem.current.alreadySelecting)
+            button.Select();
 
-	}
+    }
 
-	public void OnPointerExit(PointerEventData eventData)
-	{
-	}
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
 
-	public void OnSelect(BaseEventData eventData)
-	{
-		layoutElement.minWidth = ExpandedWidth;
-		DisplayText(true);
+    public void OnSelect(BaseEventData eventData)
+    {
+        layoutElement.minWidth = ExpandedWidth;
+        DisplayText(true);
 
-	}
+    }
 
-	public void OnDeselect(BaseEventData eventData)
-	{
-		button.OnPointerExit(null);
-		layoutElement.minWidth = MinimumWidth;
-		DisplayText(false);
+    public void OnDeselect(BaseEventData eventData)
+    {
+        button.OnPointerExit(null);
+        layoutElement.minWidth = MinimumWidth;
+        DisplayText(false);
 
-	}
-	public void UpdateText(string optionText)
-	{
-		string[] split = optionText.Split("$".ToCharArray());
-		shortText.text = split[0];
-		longText.text = split[0] + split[1];
-	}
-	void DisplayText(bool isLong)
-	{
-		longText.gameObject.SetActive(isLong);
-		shortText.gameObject.SetActive(!isLong);
-	}
+    }
+    public void UpdateText(string optionText)
+    {
+        string[] split = optionText.Split("$".ToCharArray());
+        shortText.text = split[0];
+        longText.text = split[1];
+    }
+    void DisplayText(bool isLong)
+    {
+        longText.gameObject.SetActive(isLong);
+        shortText.gameObject.SetActive(!isLong);
+    }
 
 }
