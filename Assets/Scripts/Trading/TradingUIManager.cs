1f871670eda86f7bd43d0143d72a0a1642735a03
Basile_Mathieu <basilemathieu@gmail.com>
2019-02-12 16:35:02 +0100

    TradingUIManager

---
+++ Assets/Scripts/Trading/TradingUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tfj
+{
+
+    public class TradingUIManager : Singleton<TradingUIManager>
+    {
+        public InventoryUI m_inventoryUI;
+        public GameObject m_itemButton;
+
+        // Start is called before the first frame update
+        private void Awake()
+        {
+            m_inventoryUI = tfj.TradingUIManager.Instance.m_inventoryUI;
+        }
+        public void OpenInventory()
+        {
+            m_inventoryUI.gameObject.SetActive(true);
+        }
+        public void ShowItemButton(bool _needItem)
+        {
+            m_itemButton.SetActive(_needItem);
+        }
+        public void UpdateItembutton()
+        {
+
+        }
+    }
+
+}
