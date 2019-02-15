845981441a13c0588e8bb27fb0ba2e8c9d0d3d18
LeonardA-L <leonard@allain-launay.fr>
2019-02-15 22:51:36 +0100

    Test with plato's cave

---
+++ Assets/TunnelFader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelFader : MonoBehaviour
+{
+    Animator m_anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Character")
+        {
+            m_anim.SetTrigger("Fade");
+        }
+    }
+}
