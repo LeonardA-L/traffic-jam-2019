602ed126f0f4429a3a0450b6069828a3832f1faa
Basile_Mathieu <basilemathieu@gmail.com>
2019-02-15 20:34:11 +0100

    Item spots

---
+++ Assets/Scripts/ItemSpot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using tfj;
+
+public class ItemSpot : MonoBehaviour
+{
+    private bool m_triggered = false;
+    private bool m_inRange = false;
+    public Item m_item;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        var controller = other.gameObject.GetComponent<TFJCharacterController>();
+        if (controller == null)
+        {
+            return;
+        }
+
+        if (m_triggered)
+        {
+            return;
+        }
+
+        UIManager.Instance.ShowTooltip("Pick", TooltipController.TooltipType.A); // TODO External translation file
+        m_inRange = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_inRange && !m_triggered && Input.GetButtonDown("Fire1"))   // TODO better input management
+        {
+            m_triggered = true;
+            UIManager.Instance.HideTooltip();
+            GameManager.Instance.Inventory.Add(m_item);
+            Destroy(gameObject);
+        }
+    }
+}
